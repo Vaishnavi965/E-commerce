4084666757947229e458c9f8ce3ff4d634646c52
Lahari Bhairagoni <brgni.lahari@gmail.com>
2025-06-04 21:18:04 +0530

    Initial commit

---
+++ Backend/models/Transaction.js
@@ -0,0 +1,10 @@
+//const mongoose = require('mongoose');
+
+//const transactionSchema = new mongoose.Schema({
+  //orderId: { type: mongoose.Schema.Types.ObjectId, ref: 'Order', required: true },
+  //paymentMode: { type: String, required: true }, // e.g., Card, UPI, COD
+  //paymentStatus: { type: String, default: 'Success' },
+  //transactionDate: { type: Date, default: Date.now }
+//});
+
+//module.exports = mongoose.model('Transaction', transactionSchema);
